af0c4dc3713a0ac8ecc75a842018a10cc400e800
Daniel Pérez <daniel.perez@altia.es>
2018-07-23 14:07:05 +0200

    Better help

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # HorasCLI
+
+Simple CLI program to create a project hour schedule.
+
+# Install
+pip install .
+
+# Use
+Help about use with
+horas -h or horas --help
--- horas/__main__.py
+++ horas/__main__.py
@@ -4,7 +4,8 @@ import webbrowser
 
 def ver_horas():
     chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
-    webbrowser.get(chrome_path).open('file:///C:/Users/daniel.perez/Documents/HorasCLI/horas/Horas.md')
+    webbrowser.get(chrome_path).open(
+        'file:///C:/Users/daniel.perez/Documents/HorasCLI/horas/Horas.md')
 
 
 def nuevas_horas():
@@ -22,17 +23,40 @@ def anadir_fila(nueva_fila):
 
 def main():
     if len(sys.argv) > 1:
-        if (sys.argv[1] in ['-m', '--mostrar']):
+        if (sys.argv[1] in ['-h', '--help']):
+            print("""
+                    __  __                           ______ __     ____
+                   / / / /____   _____ ____ _ _____ / ____// /    /  _/
+                  / /_/ // __ \ / ___// __ `// ___// /    / /     / /  
+                 / __  // /_/ // /   / /_/ /(__  )/ /___ / /___ _/ /   
+                /_/ /_/ \____//_/    \__,_//____/ \____//_____//___/    
+                                                       
+                Usage: 
+                horas -h | --help 'Shows this help'
+                horas -s | --show 'Open the schedule in Chrome'
+                horas -n | --new <project name> <hours> <date (xx-xx)> 'Add row to the schedule'
+            """)
+        elif (sys.argv[1] in ['-s', '--show']):
             ver_horas()
             return
-        elif (sys.argv[1] in ['-n', '--nueva']):
+        elif (sys.argv[1] in ['-n', '--new']):
             nueva_fila = nuevas_horas()
             anadir_fila(nueva_fila)
             return
     else:
-        print("Formato incorrecto prueba lo siguiente:")
-        print("horas -m")
-        print("horas -n nombre horas fecha(xx-xx)")
+        print("""
+                    __  __                           ______ __     ____
+                   / / / /____   _____ ____ _ _____ / ____// /    /  _/
+                  / /_/ // __ \ / ___// __ `// ___// /    / /     / /  
+                 / __  // /_/ // /   / /_/ /(__  )/ /___ / /___ _/ /   
+                /_/ /_/ \____//_/    \__,_//____/ \____//_____//___/    
+                                                       
+                Usage: 
+                horas -h | --help 'Shows this help'
+                horas -s | --show 'Open the schedule in Chrome'
+                horas -n | --new <project name> <hours> <date (xx-xx)> 'Add row to the schedule'
+            """)
+
 
 if __name__ == '__main__':
     main()
